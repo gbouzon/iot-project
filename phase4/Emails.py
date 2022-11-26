721177f7f0b534a9e9d9f7fbb9648e46468ffd99
danichh <70587707+danichh@users.noreply.github.com>
2022-11-26 14:45:25 -0500

    refactor email code
    Move email code to Emails file and call it through import Emails

--- phase4/Emails.py
+++ phase4/Emails.py
@@ -4,80 +4,53 @@ import smtplib
 import email
 import imaplib
 
-EMAIL = "danichhang@gmail.com";
-PASSWORD = "ivtinrmrsxoxemdr";
-SERVER = 'imap.gmail.com'
+source_address = 'pi.iotnotificationservices@gmail.com'
+dest_address = 'ga@bouzon.com.br'
+password = 'uuoe gtxq zccp yzgp'
+imap_srv = 'imap.gmail.com'
+imap_port = 993
 
-def sendEmail(email_receiver, subject ,body):
-    #variable with email sender
-    email_sender = "danichhang@gmail.com";
-    email_password = "ivtinrmrsxoxemdr";
+def send_email(subject, body):
+    smtp_srv = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_user = source_address
+    smtp_pass = password
 
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
-    em.set_content(body)
-
-    #Create a secure SSL context
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        #login into the sender email
-        smtp.login(email_sender, email_password)
-        #send the email
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-def receiveEmail(senderEmail):
-    # connect to the server and go to its inbox
-    mail = imaplib.IMAP4_SSL(SERVER)
-    mail.login(EMAIL, PASSWORD)
-    # we choose the inbox but you can select others
+    msg = 'Subject: {}\n\n{}'.format(subject, body)
+    server = smtplib.SMTP(smtp_srv, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(smtp_user, smtp_pass)
+    server.sendmail(smtp_user, dest_address, msg)
+    server.quit()
+    
+def receive_email():
+    #global emailReceived
+    mail = imaplib.IMAP4_SSL(imap_srv)
+    mail.login(source_address, password)
     mail.select('inbox')
-
-    # ALL  criteria =  retrieve every message inside the inbox
-    # return with its status and a list of ids
     status, data = mail.search(None, 
     'UNSEEN', 
-    'HEADER SUBJECT "Change Temperature Sugguestion"',
-    'HEADER FROM "' + senderEmail +  '"')
+    'HEADER SUBJECT "Temperature is High"',
+    'HEADER FROM "' + dest_address +  '"')
 
     mail_ids = []
     for block in data:
-        # the split function called without parameter
-        # transforms the text or bytes into a list using
-        # as separator the white spaces:
-        # b'1 2 3'.split() => [b'1', b'2', b'3']
         mail_ids += block.split()
-
-    # now for every id we'll fetch the email
-    # to extract its content
     for i in mail_ids:
-        # the fetch function fetch the email given its id
-        # and format that you want the message to be
         status, data = mail.fetch(i, '(RFC822)')
-
-        # the content data at the '(RFC822)' format comes on
-        # a list with a tuple with header, content, and the closing
-        # byte b')'
         for response_part in data:
             if isinstance(response_part, tuple):
-                # we go for the content at its second element
-                # skipping the header at the first and the closing
-                # at the third
                 message = email.message_from_bytes(response_part[1])
                 mail_from = message['from']
                 mail_subject = message['subject']
-
-                # then for the text we have a little more work to do
-                # because it can be in plain text or multipart
-                # if its not plain text we need to separate the message
-                # from its annexes to get the text
                 if message.is_multipart():
                     mail_content = ''
                     for part in message.get_payload():
                         if part.get_content_type() == 'text/plain':
                             mail_content += part.get_payload()
                 else:
-                    mail_content = message.get_payload()
-
-                return "yes" in mail_content.lower();
+                    mail_content = message.get_payload().lower()
+                print(mail_content)
+                return "yes" in mail_content.lower()
--- phase4/dashboard.py
+++ phase4/dashboard.py
@@ -10,6 +10,7 @@ import smtplib
 import imaplib
 import easyimap as imap
 import email
+from Emails import *  
 #import paho.mqtt.client as mqtt
 import random
 from paho.mqtt import client as mqtt_client
@@ -263,60 +264,6 @@ def create_connection(db_file):
 
     return conn
 
-def send_email(subject, body):
-    smtp_srv = 'smtp.gmail.com'
-    smtp_port = 587
-    smtp_user = source_address
-    smtp_pass = password
-
-    msg = 'Subject: {}\n\n{}'.format(subject, body)
-    server = smtplib.SMTP(smtp_srv, smtp_port)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login(smtp_user, smtp_pass)
-    server.sendmail(smtp_user, dest_address, msg)
-    server.quit()
-    
-def receive_email():
-    #global emailReceived
-    mail = imaplib.IMAP4_SSL(imap_srv)
-    mail.login(source_address, password)
-    mail.select('inbox')
-    status, data = mail.search(None, 
-    'UNSEEN', 
-    'HEADER SUBJECT "Temperature is High"',
-    'HEADER FROM "' + dest_address +  '"')
-
-    mail_ids = []
-    for block in data:
-        mail_ids += block.split()
-    for i in mail_ids:
-        status, data = mail.fetch(i, '(RFC822)')
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                message = email.message_from_bytes(response_part[1])
-                mail_from = message['from']
-                mail_subject = message['subject']
-                if message.is_multipart():
-                    mail_content = ''
-                    for part in message.get_payload():
-                        if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
-                else:
-                    mail_content = message.get_payload().lower()
-                print(mail_content)
-                return "yes" in mail_content.lower()
-                '''
-                #if 'yes' in mail_content:
-                    #return True
-                #elif 'no' in mail_content:
-                    emailReceived = False
-                    return True
-                else:
-                    return False
-                    '''
-
 @app.callback(Output('led-image', 'children'),
               Input('led-image', 'n_clicks')
                 )
@@ -464,8 +411,6 @@ def logIn():
     finally:
         cur.close()
         
-    
-
 def main():    
     client = connect_mqtt()
     subscribe(client)
